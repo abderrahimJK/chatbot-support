39ae25764a46d79a0fe14900319f860f99800853
abderrahimjk <abderrahim.aitbouna@gmail.com>
2025-10-22 19:56:18 +0100

    init commit

---
+++ src/main/java/ma/bouna_ab/chatbot_support/ChatbotSupportApplication.java
@@ -0,0 +1,13 @@
+package ma.bouna_ab.chatbot_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/bouna_ab/chatbot_support/ChatbotSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.bouna_ab.chatbot_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
